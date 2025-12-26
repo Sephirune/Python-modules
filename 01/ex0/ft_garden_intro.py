b0ea8f0c01369727c0945d41068aeceeb1aaecc0
Sephirune <sephirune@gmail.com>
2025-12-26 14:40:26 +0100

    a

---
+++ 01/ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+name = "Rose"
+height = "25cm"
+age = "30 days"
+
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}")
+    print(f"Age: {age}")
+    print("=== End of Program ===")
