74eeac388f342d4c07d218bea62f6cafce55a299
Sephirune <131598718+Sephirune@users.noreply.github.com>
2026-03-06 18:29:07 +0100

    Add files via upload

--- 09/ex1/alien_contact.py
+++ 09/ex1/alien_contact.py
@@ -1,4 +1,4 @@
-from enum import Enum, IntEnum
+from enum import Enum
 from pydantic import BaseModel, ValidationError, Field, model_validator
 from datetime import datetime
 from typing import Optional
@@ -16,9 +16,9 @@ class AlienContact(BaseModel):
     timestamp: datetime
     location: str = Field(min_length=3, max_length=100)
     contact_type: ContactType
-    signal_strength: float = Field(le=0.0, ge=10.0)
-    duration_minutes: int = Field(le=1, ge=1440)
-    witness__count: int = Field(le=1, ge=100)
+    signal_strength: float = Field(le=10.0, ge=0.0)
+    duration_minutes: int = Field(le=1440, ge=1)
+    witness__count: int = Field(le=100, ge=1)
     message_received: Optional[str] = Field(default=None, max_length=500)
     is_verified: bool = False
 
@@ -35,3 +35,47 @@ witnesses")
         if self.signal_strength > 7.0 and not self.message_received:
             raise ValueError("Strong signals should be reported!")
         return self
+
+
+def main():
+    print(
+        'Alien Contact Log Validation',
+        '\n======================================',
+        '\nValid contact report:'
+    )
+    contact = AlienContact(
+        contact_id="AC_2024_001",
+        timestamp="2026-03-06",
+        location="Area 51, Nevada",
+        contact_type="radio",
+        signal_strength=8.5,
+        duration_minutes=45,
+        witness__count=5,
+        message_received="'Greetings from Zeta Reticuli"
+    )
+    print(f"ID: {contact.contact_id}",
+          f"\nType: {contact.contact_type}",
+          f"\nLocation: {contact.location}",
+          f"\nSignal: {contact.signal_strength}/10",
+          f"\nDuration: {contact.duration_minutes}",
+          f"\nWitnesses: {contact.witness__count}",
+          f"\nMessage: {contact.message_received}"
+          )
+    print("\n======================================")
+    try:
+        contact = AlienContact(
+            contact_id="AC_2024_0014523141",
+            timestamp="2026-03-06",
+            location="Area 51, Nevada",
+            contact_type="radio",
+            signal_strength=8.5,
+            duration_minutes=45,
+            witness__count=5,
+            message_received="'Greetings from Zeta Reticuli"
+            )
+    except ValidationError as e:
+        print("Expected validation error")
+        print(e.errors()[0]["msg"])
+
+
+main()
