0993608a0b2b020fd82cc61b09af6606952db855
Aarón García García <sephirune@gmail.com>
2026-03-08 15:17:24 +0100

    bare minimum

---
+++ 10/ex2/scope_mysteries.py
@@ -0,0 +1,10 @@
+def mage_counter() -> callable:
+
+
+def spell_accumulator(initial_power: int) -> callable:
+
+
+def enchantment_factory(enchantment_type: str) -> callable:
+
+
+def memory_vault() -> dict[str, callable]:
---
+++ 10/ex4/decorator_mastery.py
@@ -0,0 +1,16 @@
+def spell_timer(func: callable) -> callable:
+
+
+def power_validator(min_power: int) -> callable:
+
+
+def retry_spell(max_attempts: int) -> callable:
+
+
+class MageGuild:
+    @staticmethod
+    def validate_mage_name(name: str) -> bool:
+    
+
+    @staticmethod
+    def cast_spell(self, spell_name: str, power: int) -> str:
